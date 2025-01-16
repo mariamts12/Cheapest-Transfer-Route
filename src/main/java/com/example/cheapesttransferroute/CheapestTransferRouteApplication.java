1b17360a138e4bf1ac96a3545d0e6a4de790864b
mariamts <matsut21@freeuni.edu.ge>
2025-01-16 17:31:52 +0400

    Initial commit

---
+++ src/main/java/com/example/cheapesttransferroute/CheapestTransferRouteApplication.java
@@ -0,0 +1,13 @@
+package com.example.cheapesttransferroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapestTransferRouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheapestTransferRouteApplication.class, args);
+    }
+
+}
